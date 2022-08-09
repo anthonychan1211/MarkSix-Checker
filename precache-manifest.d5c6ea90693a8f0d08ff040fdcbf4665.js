aaeebb6b660bda83a16697a76f4ed10fb7225fb1
Chan LongYin <Anthony@chanlongs-mbp.lan>
2022-08-09 15:50:06 +0800

    Updates

---
+++ precache-manifest.d5c6ea90693a8f0d08ff040fdcbf4665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30e025d348d715383959",
+    "url": "/MarkSix-Checker/static/css/main.59288af8.chunk.css"
+  },
+  {
+    "revision": "30e025d348d715383959",
+    "url": "/MarkSix-Checker/static/js/main.1068bcd2.chunk.js"
+  },
+  {
+    "revision": "d3aa48dacc3961b8e11d",
+    "url": "/MarkSix-Checker/static/js/runtime~main.1c34bf48.js"
+  },
+  {
+    "revision": "c3dff703ead80ed8926b",
+    "url": "/MarkSix-Checker/static/js/2.6f53e186.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/MarkSix-Checker/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "7a3a1735b8e8854b721d7a712d1695e0",
+    "url": "/MarkSix-Checker/index.html"
+  }
+];
